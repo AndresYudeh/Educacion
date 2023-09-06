b2b2f9756a10e50f6a90898b436d873604e2700d
B. Andres Yudeh Cedeño <e1315259398@live.uleam.edu.ec>
2023-09-06 17:10:30 -0500

    jsjs

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+header('location:index.php');
+
+?><!--  Orginal Author Name: CompuBinario K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at CompuBinario.infospace@gmail.com  
+ Visit website : www.CompuBinariok.com -->  

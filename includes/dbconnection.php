4f093771f91f12f404735629a2982f579c565310
B. Andres Yudeh Cedeño <e1315259398@live.uleam.edu.ec>
2023-09-10 17:36:26 -0500

    a

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,41 +1,29 @@
 <?php
 
-// define('DB_HOST', 'beduca.mysql.database.azure.com');
-// define('DB_USER', 'Andres');
-// define('DB_PASS', 'A11224477a');
-// define('DB_NAME', 'eduauth');
+define('DB_HOST', 'beduca.mysql.database.azure.com');
+define('DB_USER', 'Andres');
+define('DB_PASS', 'A11224477a');
+define('DB_NAME', 'eduauth');
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','eduauth2');
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','eduauth2');
 
 // Ruta al certificado SSL .pem
-// $sslCertPath = "pem/DigiCertGlobalRootCA.crt.pem";
+$sslCertPath = "pem/DigiCertGlobalRootCA.crt.pem";
 
-// try {
-//     $dbh = new PDO(
-//         "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
-//         DB_USER,
-//         DB_PASS,
-//         array(
-//             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
-//             // PDO::MYSQL_ATTR_SSL_CA => $sslCertPath, // Especifica la ubicación del certificado SSL
-//             // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Opcional: Desactiva la verificación del certificado del servidor (seguridad reducida)
-//         )
-//     );
-// } catch (PDOException $e) {
-//     exit("Error: " . $e->getMessage());
-// }
-
-
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+try {
+    $dbh = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+        DB_USER,
+        DB_PASS,
+        array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+            PDO::MYSQL_ATTR_SSL_CA => $sslCertPath)
+    );
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
   

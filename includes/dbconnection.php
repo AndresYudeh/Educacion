9825663295da9ae34b35fba98787d600b54b8a37
B. Andres Yudeh Cedeño <e1315259398@live.uleam.edu.ec>
2023-09-10 15:04:04 -0500

    bd

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,18 +1,26 @@
-<?php 
-// DB credentials.
-define('DB_HOST','beduca.mysql.database.azure.com');
-define('DB_USER','Andres');
-define('DB_PASS','A11224477a');
-define('DB_NAME','eduauth');
-// ssl-mode=require
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+<?php
+
+define('DB_HOST', 'beduca.mysql.database.azure.com');
+define('DB_USER', 'Andres');
+define('DB_PASS', 'A11224477a');
+define('DB_NAME', 'eduauth');
+
+// Ruta al certificado SSL .pem
+$sslCertPath = "pem/DigiCertGlobalRootCA.crt.pem";
+
+try {
+    $dbh = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+        DB_USER,
+        DB_PASS,
+        array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+            PDO::MYSQL_ATTR_SSL_CA => $sslCertPath, // Especifica la ubicación del certificado SSL
+            // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Opcional: Desactiva la verificación del certificado del servidor (seguridad reducida)
+        )
+    );
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
   

d03c64766f1c3fb9542c43626a2769a656b36b59
B. Andres Yudeh Cedeño <e1315259398@live.uleam.edu.ec>
2023-09-10 20:31:00 -0500

    alfin

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,8 +3,10 @@
 define('DB_HOST', 'beduca.mysql.database.azure.com');
 define('DB_USER', 'Andres');
 define('DB_PASS', 'A11224477a');
-define('DB_NAME', 'eduauth');
+define('DB_NAME', 'eduauth3');
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 // define('DB_HOST','localhost');
 // define('DB_USER','root');
 // define('DB_PASS','');
@@ -23,7 +25,7 @@ try {
             PDO::MYSQL_ATTR_SSL_CA => $sslCertPath)
     );
 } catch (PDOException $e) {
-    exit("Error: " . $e->getMessage());
+    error_log("Error de base de datos: " . $e->getMessage());
 }
 ?>
   
